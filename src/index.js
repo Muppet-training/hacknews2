a30278c12d6c710c509a17e58239bf07e3a6da2a
Tom <tom@Pro.local>
2018-07-11 23:10:16 +1000

    first commit

---
+++ src/index.js
@@ -0,0 +1,42 @@
+const { GraphQLServer } = require('graphql-yoga');
+const { Prisma } = require('prisma-binding');
+
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: (root, args, context, info) => {
+      return context.db.query.links({}, info);
+    }
+  },
+  Mutation: {
+    // 2
+    post: (root, args, context, info) => {
+      return context.db.mutation.createLink(
+        {
+          data: {
+            url: args.url,
+            description: args.description
+          }
+        },
+        info
+      );
+    }
+  }
+};
+
+// 3
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers,
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: 'src/generated/prisma.graphql',
+      endpoint: 'https://us1.prisma.sh/tom-7c54c9/hacknews2/dev',
+      debug: true
+    })
+  })
+});
+server.start(() =>
+  console.log(`Server is running on http://localhost:4000`)
+);
